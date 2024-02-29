5b8b1ade27285579b7a2ec0027b8397dbd497062
sama-kun <samgar.robot@gmail.com>
2024-03-01 00:30:47 +0500

    deploy and test fail

--- src/service/instanceAxios.js
+++ src/service/instanceAxios.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 // Create Axios instance
 // const baseURL = process.env.BASE_URL;
-// const baseURL = "https://plankton-app-gascv.ondigitalocean.app/api";
-const baseURL = "http://localhost:8080/api";
+const baseURL = "https://plankton-app-gascv.ondigitalocean.app/api";
+// const baseURL = "http://localhost:8080/api";
 // const baseURL = import.meta.env.BASE_URL;
 console.log("sdfksdf", baseURL);
 // Create Axios instance with base URL
